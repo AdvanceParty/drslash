9be3f12d5623cf1698a9a2d30997328891e08bb7
Gil Fewster <hello@gilfewster.com>
2020-02-27 13:08:47 +1100

    base setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <Logo className='logo' alt='Dr Slash Logo' />
       </header>
     </div>
   );
