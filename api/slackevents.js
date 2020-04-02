1c8e1e67c12db8cf24bb5a9e5d2fcbe5c5620839
Sarah Jackson <sarah@theroyals.com.au>
2020-04-02 14:45:37 +1100

    Slack events file

---
+++ api/slackevents.js
@@ -0,0 +1,15 @@
+module.exports = (req, res) => {
+  // res.json({
+  //     body: req.body,
+  //     query: req.query,
+  //     cookies: req.cookies
+  // });
+
+// Verifying slack challenge
+// console.log(req.body);
+//   res.end(
+//     req.body.challenge
+//   );
+
+
+};
